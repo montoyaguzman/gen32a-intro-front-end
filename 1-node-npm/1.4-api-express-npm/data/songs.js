02b22a58062ac89e19f6ee0df7043f0db4157fa6
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-05-15 19:43:23 -0600

    feat: ejemplo de common js

---
+++ 1-node-npm/1.4-api-express-npm/data/songs.js
@@ -0,0 +1,19 @@
+const songs = [
+    { id: 1, name: 'Kilometros', artist: 'Caligaris', year: 2007 },
+    { id: 2, name: 'Umbrella', artist: 'Rihanna', year: 2000 },
+    { id: 3, name: 'Back in black', artist: 'AC/DC', year: 1980 },
+    { id: 4, name: 'Gasolina', artist: 'Daddy Yankee', year: 2002 },
+];
+
+function sum() {
+    // do something
+}
+
+const resta = () => {
+    // doSomething
+}
+
+// common js (export)
+module.exports = {
+    songs: songs,
+};
--- 1-node-npm/1.4-api-express-npm/index.js
+++ 1-node-npm/1.4-api-express-npm/index.js
@@ -2,21 +2,15 @@ const express = require('express');
 const colors = require('colors');
 const app = express();
 const port = 3000;
-
-// songs 
-const songs = [
-    { id: 1, name: 'Kilometros', artist: 'Caligaris', year: 2007 },
-    { id: 2, name: 'Umbrella', artist: 'Rihanna', year: 2000 },
-    { id: 3, name: 'Back in black', artist: 'AC/DC', year: 1980 },
-    { id: 4, name: 'Gasolina', artist: 'Daddy Yankee', year: 2002 },
-];
+// common js (import)
+const data = require('./data/songs');
 
 app.get('/', (req, res) => {
     res.send('<html> <body> Hola desde express a todos... </body> </html>');
 });
 
 app.get('/songs', (req, res) => {
-    res.json(songs);
+    res.json(data.songs);
 });
 
 app.listen(port, () => {
