bfbcb0b09a8fc3faf117036f47a5ffd8e70314aa
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-06-03 21:28:43 -0600

    feat: agrega estilos y funcionalidad del Menu / Link

--- 5-trainers/trainers-pokemon-frontend/src/componentes/Menu/index.jsx
+++ 5-trainers/trainers-pokemon-frontend/src/componentes/Menu/index.jsx
@@ -1,8 +1,25 @@
-import './index.css'
+import './index.css';
+import logo from '../../assets/img/logo.png';
+import { Link } from 'react-router-dom';
 
 function Menu() {
     return (
-        <h1>Menu works!</h1>
+        <nav>
+            <div>
+                <img src={logo} alt="logo de trainers" className='logo-img' />
+            </div>
+            <ul className='nav-items'>
+                <Link to="/" className='nav-link'>
+                    Inicio
+                </Link>
+                <Link to="/new-trainer" className='nav-link'>
+                    Registro entrenador
+                </Link>
+                <Link to="/about" className='nav-link'>
+                    Acerca de nosotros
+                </Link>
+            </ul>
+        </nav>
     )
 }
 
