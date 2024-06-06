5f1b5d4d1ab6dbcd348502a7ab17145ec8e52737
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-06-05 20:54:50 -0600

    feat: uso del api y useEffect

--- 5-trainers/trainers-pokemon-frontend/src/pages/Home/index.jsx
+++ 5-trainers/trainers-pokemon-frontend/src/pages/Home/index.jsx
@@ -2,32 +2,65 @@ import './index.css';
 import { SearchBar } from '../../componentes/SearchBar';
 import { ListCard } from '../../componentes/ListCard';
 import { Card } from '../../componentes/Card';
+import { getTrainers } from '../../services/trainers';
+import { useState, useEffect } from 'react';
 
 function Home() {
 
-    const trainers = [
-        { name: 'ash ketchum', region: 'kanto', rank: 21 },
-        { name: 'leonel', region: 'galar', rank: 40 },
-        { name: 'james', region: 'kanto', rank: 40 },
-    ];
+    const [trainers, setTrainers] = useState([]);
+    const [counter, setCounter] = useState(0);
+
+    const getData = async () => {
+        const newTrainers = await getTrainers();
+        setTrainers(newTrainers);
+    };
+
+    const handleNumber = () => {
+        setCounter(counter + 1)
+    }
+
+    // 1. El useEffect no tiene condiciones:
+    // ===> El callback se ejecuta en cada renderizado (mount, didMount).
+    useEffect(() => {
+        console.log('Ejecutando el useEffect SIN CONDICIONES');
+    });
+
+    // 2. El useEffect tiene como condicion un []
+    // ===> El callback se ejecuta solo en el primer render (didMount).
+    useEffect(() => {
+        console.log('Ejecutando el useEffect cuando hay [] (solo la primera vez)');
+        getData();
+    }, []);
+
+    useEffect(() => {
+        console.log('Ejecutando el useEffect cuando cambia counter');
+    }, [counter]);
 
     return (
         <>
+
+            <div className="counter-container">
+                <button onClick={handleNumber}>Incrementar</button>
+                <span>El numero es: {counter}</span>
+            </div>
+
             <SearchBar />
 
             <ListCard>
 
                 {
-                    trainers.map((trainer, index) => {
-                        return (
-                            <Card
-                                key={index}
-                                name={trainer.name}
-                                region={trainer.region}
-                                rank={trainer.rank}
-                            />
-                        )
-                    })
+                    trainers.length > 0
+                        ? trainers.map((trainer, index) => {
+                            return (
+                                <Card
+                                    key={index}
+                                    name={trainer.name}
+                                    region={trainer.region.name}
+                                    rank={trainer.rank}
+                                />
+                            )
+                        })
+                        : <div>no info...</div>
                 }
 
             </ListCard>
